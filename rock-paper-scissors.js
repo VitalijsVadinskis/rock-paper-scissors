36e460f25cbd514e34fc3e14e8753506febc9714
vitalijs <117673160+VitalijsVadinskis@users.noreply.github.com>
2023-07-20 21:16:02 +0300

    Add function that decides if you win or lose

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,12 +3,38 @@ function getComputerChoice () {
     let x = Math.floor(Math.random() * 3);
     
     if (x == 0) {
-        return console.log('rock')
+        return 'rock'
     } else if (x == 1) {
-        return console.log('paper')
+        return 'paper'
     }   else {
-        return console.log('scissors')
+        return 'scissors'
     }
 }
 
-getComputerChoice ()
+function playRound (playerSelection, computerSelection) {
+    let x = playerSelection.toLowerCase()
+    let y = computerSelection.toLowerCase()
+
+    if (x == 'rock' && y == 'paper') {
+        return 'You Lose! Paper beats Rock!'
+    } else if (x == 'paper' && y == 'rock') {
+        return 'You Win! Paper beats Rock!'
+    } else if (x == 'rock' && y == 'scissors') {
+        return 'You Win! Rock beats Scissors!'
+    } else if (x == 'scissors' && y =='rock') {
+        return 'You Lose! Rock beats Scissors!'
+    } else if (x == 'paper' && y =='scissors') {
+        return 'You Lose! Scissors beats Paper!'
+    } else if (x == 'scissors' && y =='paper') {
+        return 'You Win! Scissors beats Paper!'
+    } else  
+        return 'It\'s a Tie!'
+}
+
+
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
+
