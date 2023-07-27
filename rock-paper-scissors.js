d33eced6e967dcf0ab7b0382cbf263ba8bd72d58
vitalijs <117673160+VitalijsVadinskis@users.noreply.github.com>
2023-07-27 18:51:14 +0300

    Remake function which desides the winner and logs results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,12 +1,4 @@
-const buttons = document.querySelectorAll('input')
-
-
-
-
-
-
-
-function getComputerChoice () {
+function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
     
     if (x == 0) {
@@ -18,61 +10,28 @@ function getComputerChoice () {
     }
 };
 
-function playRound (playerSelection, computerSelection) {
-    let x = playerSelection.toLowerCase()
-    let y = computerSelection.toLowerCase()
 
-    if (x == 'rock' && y == 'paper') 
-        return 'You Lose! Paper beats Rock!'
-     else if (x == 'paper' && y == 'rock') 
-        return 'You Win! Paper beats Rock!'
-     else if (x == 'rock' && y == 'scissors') 
-        return 'You Win! Rock beats Scissors!'
-     else if (x == 'scissors' && y =='rock') 
-        return 'You Lose! Rock beats Scissors!'
-     else if (x == 'paper' && y =='scissors') 
-        return 'You Lose! Scissors beats Paper!'
-     else if (x == 'scissors' && y =='paper') 
-        return 'You Win! Scissors beats Paper!'
-     else  
-        return 'It\'s a Tie!'
+function playRound () {
+
+    let playerSelection = 'rock'
+    let computerSelection = getComputerChoice()
+    let result = ''
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')) {
+
+        result = ('You Won! ' +playerSelection +' beats ' + computerSelection)
+    
+    }else if (playerSelection == computerSelection){
+        result = ('It\'s a tie! You both chose ' +playerSelection)
+    }else {
+        result = ('You Lost! ' +computerSelection +' beats ' + playerSelection)
+    }
+    return result
 };
 
-
-function game () {
-
-    const computerSelection = getComputerChoice()
-    //Sliced the string because first part tells me if i win or lose.
-    let x = (playRound(playerSelection, computerSelection)).slice(0, 5)
-    console.log(x)
-
-    if (x == 'You W') 
-        return youWon++
-    else if (x == 'You L') 
-        return pcWon++
-};
-
-function decider (youWon, pcWon) {
-game()
-game()
-game()
-game()
-game()
-    if (youWon > pcWon) 
-        console.log('YOU ARE THE CHEMPIOEN, MY FRIUEND')
-    else 
-        console.log ('You da loser my firend HHSAAHH')
-};
-
-
-let playerSelection = prompt("Rock, Paper, Scissors");
-let youWon = 0;
-let pcWon = 0;
-
-console.log('You won: ' +youWon+ ' times');
-console.log('Pc won: ' +pcWon+ ' times');
-decider(youWon, pcWon);
-
+console.log(playRound())
 
 
 
